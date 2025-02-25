aa80fefbb9bb156037bfe1b385813ed6e4ae9186
psh9002 <psh9002@gmail.com>
2025-02-25 16:24:32 +0900

    ✨: Update camera configuration in FrankaCubeLiftEnvCfg for improved clipping range and offsets

--- source/isaaclab_tasks/isaaclab_tasks/manager_based/manipulation/lift/config/franka/cam_ik_rel_env_cfg.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/manipulation/lift/config/franka/cam_ik_rel_env_cfg.py
@@ -23,6 +23,27 @@ from isaaclab_assets.robots.franka import FRANKA_PANDA_HIGH_PD_CFG  # isort: ski
 
 @configclass
 class FrankaCubeLiftEnvCfg(joint_pos_env_cfg.FrankaCubeLiftEnvCfg):
+    
+    # # Azure Kinect configuration
+    # camera = CameraCfg(
+    #     prim_path="{ENV_REGEX_NS}/Table/cam",
+    #     update_period=0.1,
+    #     height=576,
+    #     width=640,
+    #     data_types=["rgb", "depth"],
+    #     spawn=sim_utils.PinholeCameraCfg(
+    #         focal_length=0.18, focus_distance=1.2,
+    #         horizontal_aperture=0.504, vertical_aperture=0.436,
+    #         # clipping_range=(0.5, 3.86)  # Azure Kinect clipping range
+    #         clipping_range=(0.2, 1.3)  # clipping table
+    #     ),
+    #     # offset=CameraCfg.OffsetCfg(pos=(0.74, 0.0, 0.42), rot=(0.5, -0.5, 0.5, -0.5), convention="ros"),
+    #     # offset=CameraCfg.OffsetCfg(pos=(0.0, -0.86, 1.0), rot=(0.9238, 0.38268, 0.0, 0.0), convention="ros"),
+    #     # offset=CameraCfg.OffsetCfg(pos=(0.0, -0.86, 1.0), rot=(0.92388, 0.38268, 0.0, 0.0), convention="opengl"),
+    #     offset=CameraCfg.OffsetCfg(pos=(0.0, -0.2, 1.3), rot=(1.0, 0.0, 0.0, 0.0), convention="opengl"),
+    # )
+
+    # Default camera configuration
     camera = CameraCfg(
         prim_path="{ENV_REGEX_NS}/Table/cam",
         update_period=0.1,
@@ -30,14 +51,9 @@ class FrankaCubeLiftEnvCfg(joint_pos_env_cfg.FrankaCubeLiftEnvCfg):
         width=640,
         data_types=["rgb", "depth"],
         spawn=sim_utils.PinholeCameraCfg(
-            # focal_length=24.0, focus_distance=400.0, horizontal_aperture=20.955, clipping_range=(0.1, 1.0e5)
-            focal_length=0.18, focus_distance=2,
-            horizontal_aperture=0.504, vertical_aperture=0.436,
-            clipping_range=(0.5, 3.86)
+            clipping_range=(0.1, 1.3)  # clipping table
         ),
-        # offset=CameraCfg.OffsetCfg(pos=(0.74, 0.0, 0.42), rot=(0.5, -0.5, 0.5, -0.5), convention="ros"),
-        # offset=CameraCfg.OffsetCfg(pos=(0.0, -0.86, 1.0), rot=(0.9238, 0.38268, 0.0, 0.0), convention="ros"),
-        offset=CameraCfg.OffsetCfg(pos=(0.0, -0.86, 1.0), rot=(0.92388, 0.38268, 0.0, 0.0), convention="opengl"),
+        offset=CameraCfg.OffsetCfg(pos=(0.0, -0.1, 1.3), rot=(1.0, 0.0, 0.0, 0.0), convention="opengl"),
     )
 
     def __post_init__(self):
